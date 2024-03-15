2dbc41497d3e0c3082969bcaf999cf040d801dfa
Diego Caldas Salengue <dcsalengue@me.com>
2024-03-15 13:15:38 -0300

    07. Importando as fontes do projeto #1

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -1,17 +1,27 @@
+
+
 import styled from "styled-components"
 
 const ItemListaEstilizado = styled.li`
-    
-`
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 22px;
+
+    color: ${props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
+   `
 
 
 
-const ItemNavegacao = ({children, iconeAtivo, iconeInativo, ativo= false}) => {
+const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
     return (
-    <ItemListaEstilizado>
-        <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
-        {children}
-    </ItemListaEstilizado>
+        <ItemListaEstilizado $ativo={ativo}>
+            <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
+            {children}
+        </ItemListaEstilizado>
     )
 }
 
