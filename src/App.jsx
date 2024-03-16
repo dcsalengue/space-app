d956904f4cc3466c491bb55521c39fda8e54bd03
Diego Caldas Salengue <dcsalengue@me.com>
2024-03-16 19:14:59 -0300

    03. Montando o layout desktop

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,51 @@ import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
 import Banner from "./componentes/TituloEstilizado"
 import bannerBackground from './assets/banner.png'
+import Galeria from "./componentes/Galeria"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
   `
+const APPContainer = styled.div`
+  width: 1440px;
+  max-width: 100%;
+  margin: 0 auto;
 
+`
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+const ConteudoGaleria = styled.main`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
 function App() {
 
   return (
+
     <FundoGradiente>
-      <EstilosGlobais />  
-      <Cabecalho />
-      <BarraLateral />
-      <Banner
-        texto="A galeria mais completa de fotos do espaço!"
-        backgroundImage={bannerBackground}
-      />
+      <EstilosGlobais />
+      <APPContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <ConteudoGaleria>
+            <Banner
+              texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={bannerBackground}
+            />
+            <Galeria />
+          </ConteudoGaleria>
+        </MainContainer>
+
+      </APPContainer>
     </FundoGradiente>
+
   )
 }
 
