9dfb6e393b8cd0c37185f39d923b9e0e4b98fb6d
Diego Caldas Salengue <dcsalengue@me.com>
2024-03-26 15:22:18 -0300

    04. Fundo com opacidade

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -20,7 +20,7 @@ const ImagensContainer = styled.section`
     gap: 24px;
 `
 
-const Galeria = ({ fotos = [] , aoFotoSelecionada}) => {
+const Galeria = ({ fotos = [], aoFotoSelecionada }) => {
     return (
         <>
             <Tags />
@@ -29,7 +29,7 @@ const Galeria = ({ fotos = [] , aoFotoSelecionada}) => {
                     <Titulo>Navegue pela galeria</Titulo>
                     <ImagensContainer>
                         {fotos.map(foto => <Imagem 
-                            aoZoomSolicitado={aoFotoSelecionada}                        
+                            aoZoomSolicitado={aoFotoSelecionada}
                             key={foto.id} 
                             foto={foto} />)
                         }
